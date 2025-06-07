f31ccefe7169f2dcdd4d6b356d2fe7697f5ac1e6
shapaper <shapaper@126.com>
2025-06-07 19:48:33 +0800

    run apk complete

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.metaid',
+  appName: 'metaid',
+  webDir: 'dist'
+};
+
+export default config;
